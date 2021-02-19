344ede06a9293287dd367a4dcee1aacd9b9687a4
kshitiz kamal <skilite007@gmail.com>
2021-02-19 12:37:52 +0530

    add routes

---
+++ lib/routes/router.dart
@@ -0,0 +1,5 @@
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:flutter_template/presentation/core/pages/splash_page.dart';
+
+@MaterialAutoRouter(routes: [MaterialRoute(page: SplashPage, initial: true)])
+class $Router {}
---
+++ lib/routes/router.gr.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+import '../presentation/core/pages/splash_page.dart';
+
+class Routes {
+  static const String splashPage = '/';
+  static const all = <String>{
+    splashPage,
+  };
+}
+
+class Router extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.splashPage, page: SplashPage),
+  ];
+  @override
+  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, AutoRouteFactory>{
+    SplashPage: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => SplashPage(),
+        settings: data,
+      );
+    },
+  };
+}
