1f3695502e0ca11ef186b7e983ff605d3ef57a1b
kshitiz kamal <skilite007@gmail.com>
2021-07-03 23:27:50 +0530

    add internationalization support

--- lib/domain/core/constants.dart
+++ lib/domain/core/constants.dart
@@ -1,13 +1,21 @@
+import 'dart:ui';
+
 class AppConstants {
   static const String appName = "App-Name-Here";
   static const String appVersion = "1.0.0";
   static const String playStoreLink = "";
   static const String inviteText =
       "Hey! Download $appName I'm using it and It's a fast, simple and secure app. Get it at $playStoreLink";
+  static const List<Locale> supportedLocales = [
+    Locale('en', 'US'),
+  ];
+  static const String dummyProfilePictureUrl =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7MIK6s--5K3qB5E9AZ2uYAeIiBLJ9tTyRgPpl2V5V0CtKluyaa2TpINXGn4nqhWZ1Cco&usqp=CAU";
 }
 
 class StorageConstants {
-  static const String darkModeEnabled = 'dark_mode';
+  static const String themeMode = 'theme_mode';
+  static const String appLocale = 'app_locale';
   static const String fingerPrintSupported = 'fingerpring_support';
   static const String fingerPrintEnabled = 'fingerpring_enable';
 }
--- lib/presentation/core/pages/app_widget.dart
+++ lib/presentation/core/pages/app_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_template/app_localizations.dart';
 import 'package:flutter_template/application/settings/settings_bloc.dart';
 import 'package:flutter_template/domain/core/constants.dart';
 import 'package:flutter_template/injection.dart';
@@ -31,6 +33,22 @@ class AppWidget extends StatelessWidget {
               themeMode: state.appThemeMode,
               theme: AppTheme.light,
               darkTheme: AppTheme.dark,
+              supportedLocales: AppConstants.supportedLocales,
+              locale: AppConstants.supportedLocales[0],
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              localeResolutionCallback: (locale, supportedLocales) {
+                for (final supportedLocale in supportedLocales) {
+                  if (supportedLocale.languageCode == locale!.languageCode &&
+                      supportedLocale.countryCode == locale.countryCode) {
+                    return supportedLocale;
+                  }
+                }
+                return supportedLocales.first;
+              },
             );
           },
         ),
