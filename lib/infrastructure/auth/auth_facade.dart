4fc851c8c458eb6c2a1af98754b1bde70eff6a11
kshitiz kamal <skilite007@gmail.com>
2021-10-08 08:59:53 +0530

    add firebase auth code

--- lib/infrastructure/auth/auth_facade.dart
+++ lib/infrastructure/auth/auth_facade.dart
@@ -1,33 +1,64 @@
+import 'dart:developer';
+
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_template/domain/auth/auth_failure.dart';
 import 'package:flutter_template/domain/auth/i_auth_facade.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: IAuthFacade)
 class AuthFacade implements IAuthFacade {
-  AuthFacade();
+  AuthFacade(this._auth);
+  final FirebaseAuth _auth;
 
   @override
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     required String emailAddress,
     required String password,
-  }) {
-    // TODO: implement registerWithEmailAndPassword
-    throw UnimplementedError();
+  }) async {
+    try {
+      final UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return right(unit);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        log('The account already exists for that email.');
+        return left(const AuthFailure.emailAlreadyInUse());
+      }
+      return left(const AuthFailure.serverError());
+    } catch (e) {
+      log(e.toString());
+      return left(const AuthFailure.serverError());
+    }
   }
 
   @override
   Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
     required String emailAddress,
     required String password,
-  }) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
+  }) async {
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      return right(unit);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        log('Invalid email and password combination');
+        return left(const AuthFailure.invalidEmailAndPasswordCombination());
+      }
+      return left(const AuthFailure.serverError());
+    }
   }
 
   @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
+  Future<void> signOut() async {
+    await _auth.signOut();
+    log("User signed out successfully");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_template/injection.dart';
@@ -7,6 +8,7 @@ import 'package:injectable/injectable.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
+  await Firebase.initializeApp();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
