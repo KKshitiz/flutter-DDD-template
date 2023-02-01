129b20990e41b83a016d28cf676181f6e43eeca0
mhdaslam790 <mhdaslam790@gmail.com>
2023-02-01 15:34:20 +0530

    responsive

---
+++ lib/presentation/core/widgets/utility/responsive.dart
@@ -0,0 +1,47 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  }) : super(key: key);
+
+  // screen sizes
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1000 &&
+      MediaQuery.of(context).size.width >= 600;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1000;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 1000) {
+          log("desktop");
+
+          return desktop;
+        } else if (constraints.maxWidth >= 600) {
+          log("tablet");
+
+          return tablet;
+        } else {
+          log("mobile");
+
+          return mobile;
+        }
+      },
+    );
+  }
+}
